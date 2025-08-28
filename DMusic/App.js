c8a7f6239bea15d139075f8cfe1b5972894c5e0a
DanielVianaC <daniel.carvalho85@etec.sp.gov.br>
2025-08-28 12:24:22 -0300

    Codificação Parte 1 DMusic

--- DMusic/App.js
+++ DMusic/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import Slider from '@react-native-community/slider'
 
 const { width, height } = Dimensions.get('window');
 
@@ -9,6 +10,19 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.main}>
+
+        <View style={[style.imageWrapper, styles.elevation]}>
+          <Image 
+            sourse={require('./assets/img/valdemir.png')}
+            style={styles.musicImage}
+            />
+        </View>
+
+        <View>
+          <Slider></Slider>
+        </View>
+
+
 
       </View>
       <View style={styles.footer}>
@@ -36,7 +50,41 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#222831',
+  },
+  main: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  footer: {
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 25,
+    borderTopColor: '#393E45',
+    borderTopWidth: 1,
+  },
+  iconWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%'
+  },
+  imageWrapper: {
+    width: 340,
+    height: 360,
+    marginVertical: 20,
+  },
+  elevation: {
+    elevantion: 5,
+    shadowOffset: {
+      width: 5,
+      height: 5
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84
+  },
+  musicImage: {
+    width: '100%',
+    height: '100%',
+    borderRaidius: 15
   },
 });
